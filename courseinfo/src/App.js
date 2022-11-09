5fe8719f697c627419df61e9ad2028596db18341
sulymanyasir muhammad <sulymanyasir621@gmail.com>
2022-11-09 12:43:21 +0100

    wip: content

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -1,9 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+const Header = props => <h1>{props.course}</h1>
+
+const Content = props =>(
+  <div>
+    <p>
+      {props.part1} {props.exercises1}
+    </p>
+
+    <p>
+      {props.part2} {props.exercises2}
+    </p>
+
+    <p>
+      {props.part3} {props.exercises3}
+    </p>
+  </div> 
+  
+)
+
+const Total = props =>  <p>Number of exercises {props.Total}</p>
+
+  
+
 
 function App() {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
   return (
     <>
+      <Header course={course} />
+      <content part1={part1} part2={part2}  part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3 } />
+
+      <Total total={exercises1+exercises2+exercises3} />
+     
+
+      
     </>
   );
 }
