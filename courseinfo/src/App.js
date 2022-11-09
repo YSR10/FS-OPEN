0899e068ed1d958b243890fc39682f2a96d3d146
sulymanyasir muhammad <sulymanyasir621@gmail.com>
2022-11-09 12:56:50 +0100

    courseinfo

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -1,46 +1,44 @@
-const Header = props => <h1>{props.course}</h1>
+const Header = (props) => <h1>{props.course}</h1>;
 
-const Content = props =>(
+const Part = (props) => (
+  <p>
+    {props.part} {props.exercise}
+  </p>
+);
+
+const Content = (props) => (
   <div>
-    <p>
-      {props.part1} {props.exercises1}
-    </p>
+    <Part part={props.part1} exercise={props.exercises1} />
+    <Part part={props.part2} exercise={props.exercises2} />
+    <Part part={props.part3} exercise={props.exercises3} />
+  </div>
+);
 
-    <p>
-      {props.part2} {props.exercises2}
-    </p>
+const Total = (props) => <p>Number of exercises {props.Total}</p>;
 
-    <p>
-      {props.part3} {props.exercises3}
-    </p>
-  </div> 
-  
-)
-
-const Total = props =>  <p>Number of exercises {props.Total}</p>
-
-  
-
-
-function App() {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+const App = () => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
   return (
     <>
       <Header course={course} />
-      <content part1={part1} part2={part2}  part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3 } />
+      <content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
 
-      <Total total={exercises1+exercises2+exercises3} />
-     
-
-      
+      <Total total={exercises1 + exercises2 + exercises3} />
     </>
   );
-}
+};
 
 export default App;
