d7ebe2f4a01bf7bc8ceba07b7adada87d51ff428
sulymanyasir muhammad <sulymanyasir621@gmail.com>
2022-11-09 10:57:12 +0100

    feat:rendering

--- courseinfo/src/Component/Content.jsx
+++ courseinfo/src/Component/Content.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Content = () => {
-  return <div>Content</div>;
+  return (
+    <div>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+    </div>
+  );
 };
 
 export default Content;
--- courseinfo/src/Component/Total.jsx
+++ courseinfo/src/Component/Total.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Total = () => {
-  return <div>Total</div>;
+  return (
+    <div>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    </div>
+  );
 };
 
 export default Total;
