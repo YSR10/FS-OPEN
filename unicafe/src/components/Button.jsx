dd28fb4afcfec806e3943c257c56bb067093cd92
sulymanyasir muhammad <sulymanyasir621@gmail.com>
2022-11-13 12:01:00 +0100

    statistic-component

--- unicafe/src/components/Button.jsx
+++ unicafe/src/components/Button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Button = () => {
-  return <div>Button</div>;
-};
+const Button = ({ option, text, setOption }) => (
+  <button onClick={() => setOption(option + 1)}> {text} </button>
+);
 
 export default Button;
